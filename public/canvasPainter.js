c2884e9f5d78464d8d6f2630152e9812213c2a67
OpenShift guest <mr.boshambles@gmail.com>
2012-12-09 03:00:16 -0500

    refactoring paint function

--- public/canvasPainter.js
+++ public/canvasPainter.js
@@ -1,10 +1,10 @@
-function paintCircle(x, y) {
-    var world = document.getElementById("world");
-    var kontext = world.getContext("2d");
-    kontext.beginPath();
-    kontext.arc(x, y, 3.5, 0, 2*Math.PI);
-    kontext.strokeStyle = 'blue';
-    kontext.stroke();
-    kontext.fillStyle = '#8ED6FF';
-    kontext.fill();
+function paintCircle(canvasId, x, y) {
+    var canvas = document.getElementById(canvasId);
+    var context = canvas.getContext("2d");
+    context.beginPath();
+    context.arc(x, y, 3.5, 0, 2*Math.PI);
+    context.strokeStyle = 'blue';
+    context.stroke();
+    context.fillStyle = '#8ED6FF';
+    context.fill();
 }
